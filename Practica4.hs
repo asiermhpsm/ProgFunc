4d77c61d7f0a84ed4b46c22dba1ea2418dfe24f8
asiermhpsm <118305795+asiermhpsm@users.noreply.github.com>
2023-05-18 22:01:06 +0200

    Update Practica4.hs

--- Practica4.hs
+++ Practica4.hs
@@ -4,7 +4,8 @@ import Data.List
 {-
 Funcionamiento general del codigo:
 Primero se generarán toda las combinaciones posibles con las letras disponibles. A continuación se eliminaran las que no cumplan unas características generales (como que no es posible que haya 4 consonantes seguidas) 
-y luego se comprobará que cumplan otras reglas relacionadas a las sílabas (como que consonantes al final de sílaba solo puede ser l, n, s,r). Estas reglas de las silabas se harán en profundiad, es decir que dada la palabra se intentara separar en sílabas 
+y luego se comprobará que cumplan otras reglas relacionadas a las sílabas (como que consonantes al final de sílaba solo puede ser l, n, s,r). 
+Estas reglas de las silabas se harán en profundiad, es decir que dada la palabra se intentara separar en sílabas 
 de todas la maneras posibles de manera que cumplan con las reglas llegando al final de una separación antes de probar con otra y si encuentra una separación que satisface las reglas no se busca más.
 Reglas generales:
 -No puede haber 4 consonantes seguidas
